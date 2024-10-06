1fdcc015159bf5429eca34dcb70b27ee227aa1e3
fancn21th <fancn21th@gmail.com>
2024-10-06 20:57:40 +0800

    📦 NEW: a simple button

---
+++ example/simple/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "my-component-library";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Component Library</h1>
+      <Button label="Click Me" />
+    </div>
+  );
+}
+
+export default App;
